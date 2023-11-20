a57d57efbcf4c3f74fefcb3b4324ac962de33927
hazveg <133221043+hazveg@users.noreply.github.com>
2023-11-20 16:55:52 +0100

    oops symlinks didn't work, fixed it now

--- install.sh
+++ install.sh
@@ -13,10 +13,13 @@ then
 	ln -s ~/.dotfiles/linux/nvim ~/.config/nvim
 	echo "Creating symlink for ~/.local/share/konsole"
 	ln -s ~/.dotfiles/linux/konsole ~/.local/share/konsole
-	echo "Creating symlink for ~/.config/khotkeysrc"
-	ln -s ~/.dotfiles/linux/khotkeysrc ~/.config/khotkeysrc
-	echo "Creating symlink for ~/.config/kglobalshortcutsrc"
-	ln -s ~/.dotfiles/linux/konsole ~/.config/kglobalshortcutsrc
+	#echo "Creating symlink for ~/.config/khotkeysrc"
+	#ln -s ~/.dotfiles/linux/khotkeysrc ~/.config/khotkeysrc
+	#echo "Creating symlink for ~/.config/kglobalshortcutsrc"
+	#ln -s ~/.dotfiles/linux/konsole ~/.config/kglobalshortcutsrc
+	# FOR WHATEVER REASON, KDE REFUSES TO READ IN SYMLINKS, SO WE GOTTA COPY AND UPDATE MANUALLY
+	echo "Setting KDE Shortcuts"
+	cp ~/.dotfiles/linux/khotkeysrc ~/.dotfiles/linux/kglobalshortcutsrc ~/.config
 else
 	echo "Installation cancelled by user"
 fi
