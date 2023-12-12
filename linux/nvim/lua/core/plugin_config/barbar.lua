b4bb2a5add5f3f86511c46d7a1070f5c56e4cd1e
hazveg <133221043+hazveg@users.noreply.github.com>
2023-12-12 10:55:39 +0100

    got tab switching working, adjusted konsole accordingly

--- linux/nvim/lua/core/plugin_config/barbar.lua
+++ linux/nvim/lua/core/plugin_config/barbar.lua
@@ -1,8 +1,6 @@
-local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
--- Don't work, will have to click for now.
--- map('n', '<C-S-<Tab>>', '<Cmd>BufferPrevious<CR>', opts)
--- map('n', '<C-<Tab>>', '<Cmd>BufferNext<CR>', opts)
+vim.keymap.set('n', '<C-Tab>', '<Cmd>BufferNext<CR>', opts)
+vim.keymap.set('n', '<C-S-Tab>', '<Cmd>BufferPrevious<CR>', opts)
 
-map('n', '<C-w>', '<Cmd>BufferClose<CR>', opts)
+vim.keymap.set('n', '<C-w>', '<Cmd>BufferClose<CR>', opts)
